d22742a9fc6392b5d3a4cde3407e2b8b7c5ff673
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-15 16:22:44 +0700

    Creating an user access token

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const JWT = require("jsonwebtoken");
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+const users = [
+  {
+    id: 1,
+    username: "Kai",
+  },
+  {
+    id: 2,
+    username: "Cai",
+  },
+];
 
 app.get("/posts", (req, res) => {
   res.status(200).json({ posts: "my posts" });
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  const user = users.find((user) => user.username === username);
+  console.log(user);
+
+  if (!user) return res.sendStatus(401);
+
+  // Create JWT
+  const accessToken = JWT.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  return res.status(200).json({ accessToken });
 });
 
 app.listen(PORT, () => {
