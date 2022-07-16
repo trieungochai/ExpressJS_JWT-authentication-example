dab6fa498c5435aa9ac32617090053e61906d688
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-07-16 14:10:26 +0700

    Verifying a JSON web token

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const JWT = require("jsonwebtoken");
+const verifyToken = require("./middleware/auth");
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+// data
 const users = [
   {
     id: 1,
@@ -17,14 +19,32 @@ const users = [
   },
 ];
 
-app.get("/posts", (req, res) => {
-  res.status(200).json({ posts: "my posts" });
+const posts = [
+  {
+    userId: 1,
+    post: "Kai's Post",
+  },
+  {
+    userId: 2,
+    post: "Cai's post",
+  },
+  {
+    userId: 2,
+    post: "Chi's post",
+  },
+];
+
+// app
+app.get("/posts", verifyToken, (req, res) => {
+  // res.status(200).json({ posts: "my posts" });
+  return res
+    .status(200)
+    .json(posts.filter((post) => (post.userId === req.userId)));
 });
 
 app.post("/login", (req, res) => {
   const { username } = req.body;
   const user = users.find((user) => user.username === username);
-  console.log(user);
 
   if (!user) return res.sendStatus(401);
 
